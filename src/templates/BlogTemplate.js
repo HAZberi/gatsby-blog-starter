72528371e81852ba3eacf1dede15dd675766d002
HAZberi <mhazuberi@gmail.com>
2021-03-03 18:27:14 -0700

    custom styles to gatsbyImage

--- src/templates/BlogTemplate.js
+++ src/templates/BlogTemplate.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layout from "../components/Layout.js"
 import { graphql } from "gatsby"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
-import {BLOCKS} from "@contentful/rich-text-types";
+import { BLOCKS } from "@contentful/rich-text-types"
 import { GatsbyImage } from "gatsby-plugin-image"
 
 export const query = graphql`
@@ -19,8 +19,8 @@ export const query = graphql`
       publishedDate(formatString: "MMMM Do, YYYY")
       body {
         raw
-        references{
-          file{
+        references {
+          file {
             url
           }
           description
@@ -46,23 +46,35 @@ const BlogTemplate = props => {
         </div>
       )
     if (props.data.contentfulBlogPosts) {
-      const bodyContent = JSON.parse(props.data.contentfulBlogPosts.body.raw);
-      const references = props.data.contentfulBlogPosts.body.references;
+      const bodyContent = JSON.parse(props.data.contentfulBlogPosts.body.raw)
+      const references = props.data.contentfulBlogPosts.body.references
       //if blog posts contained images or if we want to customized a specific
       //node we have to drill down the node and use ids as references
       //especially in the case of images
-      //without options object bodyContent 
+      //without options object bodyContent
       const options = {
         renderNode: {
           [BLOCKS.EMBEDDED_ASSET]: node => {
-            const imageId=node.data.target.sys.id
-            const referenceDetail=references.find(({contentful_id: id}) => id === imageId);
-            const alt = referenceDetail.title;
+            const imageId = node.data.target.sys.id
+            const referenceDetail = references.find(
+              ({ contentful_id: id }) => id === imageId
+            )
+            const alt = referenceDetail.title
             //GatsbyImage component requires you to pass image config as prop
-            //if images are coming in dynamically - then we have to query 
+            //if images are coming in dynamically - then we have to query
             //gatsbyImageData in GraphQl and set it as image prop to GatsbyImage component
-            const image = referenceDetail.gatsbyImageData;
-            return <GatsbyImage alt={alt} image={image}/>
+            const image = referenceDetail.gatsbyImageData
+            return (
+              <GatsbyImage
+                alt={alt}
+                image={image}
+                style={{
+                  alignSelf: "center",
+                  borderRadius: "15px",
+                  marginBottom: "1.25em",
+                }}
+              />
+            )
           },
         },
       }
@@ -70,7 +82,9 @@ const BlogTemplate = props => {
         <div style={{ maxWidth: "50em", margin: "0 auto" }}>
           <h1>{props.data.contentfulBlogPosts.title}</h1>
           <p>Published On: {props.data.contentfulBlogPosts.publishedDate}</p>
-          <div>{documentToReactComponents(bodyContent, options)}</div>
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            {documentToReactComponents(bodyContent, options)}
+          </div>
         </div>
       )
     }
